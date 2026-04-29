cfc5816faffdec41af52cbdd71446327f1169117
tedevirilassa <mrdownload@gmail.com>
2026-04-29 10:03:27 +0200

    modifica database url

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -4,8 +4,10 @@ const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
 
 function createPrismaClient() {
+  const isProduction = process.env.NODE_ENV === "production";
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
   });
   return new PrismaClient({ adapter });
 }
